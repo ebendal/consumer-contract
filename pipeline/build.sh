59c8e0e968de575be97353acf2886898f872b155
Reggie Ebendal <development@ebendal.com>
2022-07-19 16:19:11 +0200

    Decode sign key

--- pipeline/build.sh
+++ pipeline/build.sh
@@ -3,7 +3,7 @@ set -e -o pipefail -o nounset
 
 APPLICATION_VERSION=$(cat application-version.txt)
 echo "Application version: $APPLICATION_VERSION"
-
+SIGN_KEY=$(echo $SIGN_KEY | base64 -d)
 mvn -Drevision="$APPLICATION_VERSION" -q -s ./pipeline/settings.xml -pl '!example' clean deploy
 APPLICATION_NAME=$(mvn help:evaluate -Dexpression=project.name -q -DforceStdout)
 echo "Application name: $APPLICATION_NAME"
