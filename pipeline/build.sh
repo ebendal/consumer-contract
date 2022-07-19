ce6c56adf1936973c70189f6b2effa94ac9ad751
Reggie Ebendal <development@ebendal.com>
2022-07-19 14:08:27 +0200

    Fix maven command

--- pipeline/build.sh
+++ pipeline/build.sh
@@ -4,7 +4,7 @@ set -e -o pipefail -o nounset
 APPLICATION_VERSION=$(cat application-version.txt)
 echo "Application version: $APPLICATION_VERSION"
 
-mvn -Drevision="$APPLICATION_VERSION" -q -s ./pipeline/settings.xml -pl -example clean deploy
+mvn -Drevision="$APPLICATION_VERSION" -q -s ./pipeline/settings.xml -pl '!example' clean deploy
 APPLICATION_NAME=$(mvn help:evaluate -Dexpression=project.name -q -DforceStdout)
 echo "Application name: $APPLICATION_NAME"
 
